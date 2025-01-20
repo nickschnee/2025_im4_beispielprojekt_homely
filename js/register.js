bbab2b0504dd237d6641b2d23f8ccf8a2dfdcce9
Nick Schneeberger <nick.schneeberger@icloud.com>
2025-01-20 15:47:17 +0100

    :sparkles: update register page with username

--- js/register.js
+++ js/register.js
@@ -4,6 +4,7 @@ document
   .addEventListener("submit", async (e) => {
     e.preventDefault();
 
+    const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
@@ -11,7 +12,7 @@ document
       const response = await fetch("api/register.php", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams({ email, password }),
+        body: new URLSearchParams({ name, email, password }),
       });
       const result = await response.json();
 
